1d1b7a43b38739f014828853364b0b08eb3745ad
Alejandro Canizares <63413161+alejocv7@users.noreply.github.com>
2023-03-20 18:13:07 -0700

    Added tic_tac_toe_ui implementation

---
+++ src/tic_tac_toe/board.py
@@ -0,0 +1,81 @@
+from typing import Literal
+
+
+class Board:
+    ROWS, COLS = 3, 3
+
+    def __init__(self) -> None:
+        self.board = self.get_clean_board()
+
+    def clean(self) -> None:
+        """Reset the game board"""
+        self.board = self.get_clean_board()
+
+    def get_clean_board(self) -> list[list[str | int]]:
+        """Generate a clean board"""
+        return [
+            [(row * self.COLS) + col + 1 for col in range(self.COLS)]
+            for row in range(self.ROWS)
+        ]
+
+    def show(self) -> None:
+        """Prints the game board"""
+        print(self.board)
+
+    def is_valid_row_col(self, row: int, col: int) -> bool:
+        """Checks whether a new move is valid based on board index and occupied spots
+
+        Args:
+            move (int): Player move
+
+        Returns:
+            bool: Whether the move is within board's len and not on an ocupied spot
+        """
+        return (
+            0 <= row < self.ROWS
+            and 0 <= col < self.COLS
+            and self.board[row][col] not in {"X", "O"}
+        )
+
+    def insert_mark(self, mark: Literal["X", "O"], row: int, col: int) -> None:
+        if not self.is_valid_row_col(row, col):
+            raise ValueError
+        self.board[row][col] = mark
+
+    def check_win(self) -> bool:
+        """Checks if a player has won the game
+
+        Returns:
+            bool: Whether a player has won
+        """
+
+        # Check rows
+        for row in self.board:
+            if len(set(row)) == 1:
+                return True
+
+        # Check columns
+        for col in range(self.COLS):
+            if len(set(row[col] for row in self.board)) == 1:
+                return True
+
+        # Check diagonals
+        if len(set(self.board[i][i] for i in range(self.ROWS))) == 1:
+            return True
+        if len(set(self.board[i][self.COLS - i - 1] for i in range(self.ROWS))) == 1:
+            return True
+
+        return False
+
+    def check_tie(self) -> bool:
+        """Checks if the game has ended on a tie
+
+        Returns:
+            bool: Whether a tie exists
+        """
+
+        for row in self.board:
+            for mark in row:
+                if mark not in {"X", "O"}:
+                    return False
+        return True
--- src/tic_tac_toe/tic_tac_toe_ui.py
+++ src/tic_tac_toe/tic_tac_toe_ui.py
@@ -1,122 +1,132 @@
+import enum
+from typing import Literal
+
+import board
 import pygame as pg
 
-import tic_tac_toe
+WIN_SIZE = WIDTH, HEIGHT = pg.Vector2(650)
+TILE_SIZE = WIN_SIZE // 3
+MARK_SIZE = WIN_SIZE // 4
+MARK_PADDING = (TILE_SIZE - MARK_SIZE) // 2
+
+BOARD_IMG = pg.transform.scale(pg.image.load("assets/graphics/board.png"), WIN_SIZE)
+X_SYMBOL = pg.transform.scale(pg.image.load("assets/graphics/board_x.png"), MARK_SIZE)
+O_SYMBOL = pg.transform.scale(pg.image.load("assets/graphics/board_o.png"), MARK_SIZE)
 
 
-class TicTacToe(tic_tac_toe.TicTacToe):
-    ROW_CNT, COL_CNT = 3, 3
-    WIN_SIZE = WIDTH, HEIGHT = pg.Vector2(650)
-    TILE_SIZE = WIN_SIZE // 3
-    MARK_SIZE = WIN_SIZE // 4
-    MARK_PADDING = (TILE_SIZE - MARK_SIZE) // 2
+class State(enum.Enum):
+    """Enum for the Game's State Machine"""
 
-    def __init__(self) -> None:
+    initialized = "initialized"  # Pygame is configured, images are loaded, etc
+    game_play = "game_play"  # Players are still making moves
+    check_game_over = "check_game_over"
+    game_ended = "game_ended"  # Rendering the game ended screen
+
+
+class Board(board.Board):
+    def __init__(self, screen: pg.Surface) -> None:
         super().__init__()
+        self.screen = screen
 
-        pg.init()
-        pg.display.set_caption("Tic-Tac-Toe")
-        self.screen = pg.display.set_mode(self.WIN_SIZE)
-        self.FONT = pg.font.Font(None, 50)
+    def show(self) -> None:
+        self.screen.blit(BOARD_IMG, (0, 0))
 
-        self.board_surface = pg.transform.scale(
-            pg.image.load("assets/graphics/board.png"), self.WIN_SIZE
-        )
-        self.player_x = pg.transform.scale(
-            pg.image.load("assets/graphics/board_x.png"),
-            self.MARK_SIZE,
-        )
-        self.player_o = pg.transform.scale(
-            pg.image.load("assets/graphics/board_o.png"),
-            self.MARK_SIZE,
-        )
-
-        self.running = True
-
-    def get_rendered_txt(self, text: str) -> pg.Surface:
-        return self.FONT.render(text, True, "Black", "White")
-
-    def get_mark_pos_from_row_col(self, row: int, col: int) -> tuple[float, float]:
-        return (
-            self.TILE_SIZE[0] * col + self.MARK_PADDING[0],
-            self.TILE_SIZE[1] * row + self.MARK_PADDING[1],
-        )
-
-    def get_row_col_from_mouse(self, mouse_pos: tuple[int, int]) -> tuple[int, int]:
-        posx, posy = mouse_pos
-        return (
-            int(posy // self.TILE_SIZE[0]),
-            int(posx // self.TILE_SIZE[1]),
-        )
-
-    def print_board(self) -> None:
-        self.screen.blit(self.board_surface, (0, 0))
-
-        for row in range(self.ROW_CNT):
-            for col in range(self.COL_CNT):
+        for row in range(self.ROWS):
+            for col in range(self.COLS):
                 mark_pos = self.get_mark_pos_from_row_col(row, col)
 
                 if self.board[row][col] == "X":
-                    self.screen.blit(self.player_x, mark_pos)
+                    self.screen.blit(X_SYMBOL, mark_pos)
                 elif self.board[row][col] == "O":
-                    self.screen.blit(self.player_o, mark_pos)
+                    self.screen.blit(O_SYMBOL, mark_pos)
 
-    def get_next_player_move(self) -> tuple[int, int]:
-        row, col = self.get_row_col_from_mouse(pg.mouse.get_pos())
-        if self.is_valid_idx(row, col):
-            return row, col
-        return -1, -1
-
-    def should_play_again(self) -> bool:
-        msg = self.get_rendered_txt("Press space or click to play again!")
-        self.screen.blit(
-            msg, msg.get_rect(midtop=(self.WIN_SIZE + (0, msg.get_height())) // 2)
+    def get_mark_pos_from_row_col(self, row: int, col: int) -> tuple[float, float]:
+        return (
+            TILE_SIZE[0] * col + MARK_PADDING[0],
+            TILE_SIZE[1] * row + MARK_PADDING[1],
         )
-        return pg.key.get_pressed()[pg.K_SPACE] or pg.mouse.get_pressed()
 
-    def draw(self) -> None:
-        self.print_board()
-        pg.display.update()
+    def get_row_col_from_mouse(self, mouse_pos: tuple[int, int]) -> tuple[int, int]:
+        posy, posx = mouse_pos
+        return int(posx // TILE_SIZE[0]), int(posy // TILE_SIZE[1])
+
+
+class TicTacToe:
+    def __init__(self) -> None:
+        pg.init()
+        pg.display.set_caption("Tic-Tac-Toe")
+
+        self.screen = pg.display.set_mode(WIN_SIZE)
+        self.player: Literal["X", "O"] = "X"
+        self.board = Board(self.screen)
+        self.FONT = pg.font.Font(None, 50)
+        self.state = State.initialized
 
     def run(self) -> None:
+        self.board.show()
+        self.state = State.game_play
+
         while True:
             for event in pg.event.get():
-                if event.type == pg.QUIT or (
-                    (pg.key.get_mods() & pg.KMOD_CTRL) and event.key == pg.K_w
+                if (
+                    event.type == pg.QUIT
+                    or (pg.key.get_mods() & pg.KMOD_CTRL)
+                    and event.key == pg.K_w
                 ):
                     pg.quit()
                     exit()
 
-                if self.running:
-                    self.print_board()
-                    if event.type == pg.MOUSEBUTTONDOWN:
-                        player_move = self.get_next_player_move()
-                        if player_move == (-1, -1):
-                            continue
-
-                        row, col = player_move
-                        self.board[row][col] = self.player
-
-                        if self.check_win() or (tie := self.check_tie()):
-                            self.running = False
-                            self.print_board()
-
-                            msg = (
-                                "It's a tie!"
-                                if tie
-                                else f"Player '{self.player}', you win!"
-                            )
-                            msg_surface = self.get_rendered_txt(f"***** {msg} *****")
-                            self.screen.blit(
-                                msg_surface,
-                                msg_surface.get_rect(center=self.WIN_SIZE // 2),
-                            )
-                        self.change_player()
-
-                elif self.should_play_again():
-                    self.running = True
-                    self.board = self.get_clean_board()
+                if self.state == State.game_play:
+                    self.handle_player_move_event(event)
+                elif self.state == State.check_game_over:
+                    self.handle_game_over_check()
+                    self.change_player()
+                elif self.state == State.game_ended:
+                    self.handle_game_over(event)
 
             pg.display.update()
+
+    def handle_player_move_event(self, event: pg.event.Event) -> None:
+        if event.type == pg.MOUSEBUTTONDOWN:
+            row, col = self.board.get_row_col_from_mouse(pg.mouse.get_pos())
+            try:
+                self.board.insert_mark(self.player, row, col)
+            except ValueError:
+                # This happens if the spot is occupied or row/col are out of bounds.
+                # For the most part, row/col should be inside the game screen, since
+                # that is how clicks are registered. But, for whatever reason, sometimes
+                # pygame adds a clickable frame causing the input to be out of bounds
+                return
+            self.state = State.check_game_over
+            self.board.show()
+
+    def handle_game_over_check(self) -> None:
+        if (win := self.board.check_win()) or self.board.check_tie():
+            self.state = State.game_ended
+            self.board.show()
+
+            msg = f"Player '{self.player}', you win!" if win else "It's a tie"
+            msg_surface = self.render_txt(f"***** {msg} *****")
+            self.screen.blit(msg_surface, msg_surface.get_rect(center=WIN_SIZE // 2))
+        else:
+            self.state = State.game_play
+
+    def handle_game_over(self, event: pg.event.Event) -> None:
+        msg = self.render_txt("Press space or click to play again!")
+        self.screen.blit(
+            msg, msg.get_rect(midtop=(WIN_SIZE + (0, msg.get_height())) // 2)
+        )
+
+        if event.type == pg.MOUSEBUTTONDOWN:
+            self.state = State.game_play
+            self.board.clean()
+            self.board.show()
+
+    def render_txt(self, text: str) -> pg.Surface:
+        return self.FONT.render(text, True, "Black", "White")
+
+    def change_player(self) -> None:
+        self.player = "O" if self.player == "X" else "X"
 
 
 if __name__ == "__main__":
